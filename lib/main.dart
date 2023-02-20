6df83434a4ce210745eda88df3554904eccdc362
Sussan Anukem <sussananukem20@gmail.com>
2023-02-20 21:49:20 +0000

    Restarted Work for better understanding

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,6 @@
 import 'package:device_preview/device_preview.dart';
 import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
-import 'home.dart';
 
 void main() {
   runApp(
@@ -22,15 +21,23 @@ class MyApp extends StatelessWidget {
       locale: DevicePreview.locale(context),
       builder: DevicePreview.appBuilder,
       title: 'BMI Calculator Application',
-      theme: ThemeData.dark().copyWith(
-        primaryColor: const Color(0xFF0A0E21),
-        scaffoldBackgroundColor: const Color(0xFF0A0E21),
-        appBarTheme: const AppBarTheme(
-          backgroundColor: Color(0xFF0A0E21),
-        )
-      ),
+      theme: ThemeData.dark(),
       home: const MyHomePage(),
     );
   }
 }
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({Key? key}) : super(key: key);
+
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
+
 
